41bedf73eb7eb8213e8ba36ac257dd43ad1858a0
patrick <patrick@korczewski.de>
2024-09-14 14:03:37 +0200

    4

--- docker/docker_install.sh
+++ docker/docker_install.sh
@@ -10,3 +10,5 @@ mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 echo "Logout and then log back in to make the user account group active"
+
+#sudo nano /etc/docker/daemon.json
