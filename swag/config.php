585e84181abd5ae0c12f05477f401fd224312f8a
patrick <patrick@korczewski.de>
2024-09-20 00:48:06 +0200

    4

---
+++ swag/config.php
@@ -0,0 +1,4 @@
+'trusted_proxies' => ['172.17.0.1'],  // Docker's default bridge IP
+'overwrite.cli.url' => 'https://nextcloud.korczewski.de',
+'overwritehost' => 'nextcloud.korczewski.de',
+'overwriteprotocol' => 'https',
